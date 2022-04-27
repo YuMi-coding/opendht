94ed15d7b6ce87c1d70345501e734cdbb171fa94
Yu Mi <yxm319@case.edu>
2022-04-27 17:50:47 -0400

    lazy commit

--- tools/tools_common.h
+++ tools/tools_common.h
@@ -164,7 +164,13 @@ getDhtConfig(dht_params& params)
     config.dht_config.id = params.id;
     config.dht_config.cert_cache_all = static_cast<bool>(params.id.first);
     config.dht_config.node_id = params.node_id;
-    // std::cout << "Reading node id from params " << params.node_id<< std::endl;
+    if(config.dht_config.node_id.size() < 2*HASH_LEN)
+    {
+        __u32 diff_len = 2*HASH_LEN - config.dht_config.node_id.size();
+        for(__u32 i = 0; i< diff_len; i++)
+            config.dht_config.node_id = "0" + config.dht_config.node_id;
+    }
+    std::cout << "Reading node id from params " << params.node_id<< std::endl;
     config.threaded = true;
     config.proxy_server = params.proxyclient;
     config.push_node_id = "dhtnode";
